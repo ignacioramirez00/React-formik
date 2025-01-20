6f9fd29e8caf46dc41a81e5b781b93d736e3046f
ignacioramirez00 <ignacio.unicen@gmail.com>
2025-01-20 00:30:34 -0300

    validaciones de campos

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useFormik } from "formik";
 
 function App() {
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      lastname: "",
+      email: "",
+    },
+    validate: (values) => {
+      const errors = {};
+      if (!values.name) {
+        errors.name = "Requerido";
+      } else if (values.name.length < 5) {
+        errors.name = "El nombre es muy corto";
+      }
+
+      if (!values.lastname) {
+        errors.lastname = "Requerido";
+      } else if (values.lastname.length < 5) {
+        errors.lastname = "El apellido es muy corto";
+      }
+
+      return errors;
+    },
+    onSubmit: (values) => console.log(values),
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor="name">Nombre</label>
+      <input
+        id="name"
+        name="name"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.name}
+      />
+      {formik.errors.name ? <div>{formik.errors.name}</div> : null}
+      <br />
+      <label htmlFor="lastname">Apellido</label>
+      <input
+        id="lastname"
+        name="lastname"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.lastname}
+      />
+      {formik.errors.lastname ? <div>{formik.errors.lastname}</div> : null}
+      <br />
+      <label htmlFor="email">Email</label>
+      <input
+        id="email"
+        name="email"
+        type="email"
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
